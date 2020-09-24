17eb38ea22e09e65661842e02aebf647ca219a47
Ravi Kotecha <kotecha.ravi@gmail.com>
2020-09-24 16:16:55 +0100

    Experimental: Make back links just press "browser back"
    Instead of trying to be clever with the back link, just do what the browser
    would do.
    
    This is an experimental branch and should not be merged.

--- shared_assets/lite-frontend/javascripts/back-link.js
+++ shared_assets/lite-frontend/javascripts/back-link.js
@@ -1,9 +1,5 @@
 $(".govuk-back-link").on("click", function() {
 	address = $(this).attr("href");
-	if (address != "#") {
-		window.location.href = address;
-	} else {
-		window.history.go(-1);
-	}
+		window.history.back();
 	return false;
 });
